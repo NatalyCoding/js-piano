0cdbd73e3fec736fe7f2be2d89b4f4444a1a8982
Nataly Panakhyd <natalycoding@gmail.com>
2021-03-22 01:28:45 +0200

    feat: add playing with keys script

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const keys = document.querySelectorAll('.key');
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
 
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key))
+})
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return;
+    const key = e.key;
+    const whiteKeysIndex = WHITE_KEYS.indexOf(key);
+    const blackKeysIndex = BLACK_KEYS.indexOf(key);
+
+    if (whiteKeysIndex > -1) playNote(whiteKeys[whiteKeysIndex]);
+    if (blackKeysIndex > -1) playNote(blackKeys[blackKeysIndex])
 })
 
 function playNote(key) {
